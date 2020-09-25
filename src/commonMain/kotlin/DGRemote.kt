3992abcf6e07c25eab50149d691d7576432eeee2
sound-reload <wmliyin@outlook.com>
2020-09-26 03:58:03 +0800

    实现：使用一个特定值分辨协议

--- src/commonMain/kotlin/DGRemote.kt
+++ src/commonMain/kotlin/DGRemote.kt
@@ -1,7 +1,11 @@
 class DGRemote {
     companion object {
-        public fun identifyProtocolVersion(data: String) {
-
+        public fun identifyProtocolVersion(data: String) : String {
+            return if (data.contains("aStrengthRangeMax")) {
+                "V2"
+            } else {
+                "V1"
+            }
         }
     }
 }
