807f6f763bd28b9255da41607a8710d025df2880
sound-reload <wmliyin@outlook.com>
2020-09-26 03:58:34 +0800

    实现：导出函数

--- src/commonMain/kotlin/DGRemote.kt
+++ src/commonMain/kotlin/DGRemote.kt
@@ -1,5 +1,10 @@
+import kotlin.js.JsName
+import kotlin.jvm.JvmStatic
+
 class DGRemote {
     companion object {
+        @JvmStatic
+        @JsName("identifyProtocolVersion")
         public fun identifyProtocolVersion(data: String) : String {
             return if (data.contains("aStrengthRangeMax")) {
                 "V2"
